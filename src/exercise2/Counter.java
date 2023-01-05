0b1d719fb3c881240e4172aab11be3fa31888873
George Clam <gclam77@gmail.com>
2023-01-05 16:40:25 -0700

    Sole commit

--- src/exercise2/Counter.java
+++ src/exercise2/Counter.java
@@ -12,20 +12,17 @@ package exercise2;
 @since Jan 05 2023
 @updates: ****/
 
-public class Numbers {
+public class Counter {
 	
 	/**
-	 * Executes all the methods in the class
+	 * Executes all the methods in the Number class
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		Numbers num = new Numbers();
+		Counter num = new Counter();
 		
 		num.countToTen();
-		System.out.println("");
 		num.countFromTen();
-		System.out.println("");
 		num.evenNumbers();
 	}
 	
@@ -34,31 +31,33 @@ public class Numbers {
 	 */
 	public void countToTen() {
 		int counter = 1;
-		System.out.println("This method counts to ten using a while loop!");
+		System.out.println("This method counts to ten using a while loop:");
 		while (counter < 11) {
-			System.out.println(counter);
+			System.out.print(counter + " ");
 			counter++;
 		}
+		System.out.println();
 	}
 	
 	/**
 	 * Counts down from 10 using a for loop.
 	 */
 	public void countFromTen() {
-		System.out.println("This method counts down from ten using a for loop!");
+		System.out.println("This method counts down from ten using a for loop:");
 		for (int i = 10; i > 0; i--) {
-			System.out.println(i);
+			System.out.print(i + " ");
 		}
+		System.out.println();
 	}
 	
 	/**
 	 * Counts to twenty by twos using a for loop and an if statement
 	 */
 	public void evenNumbers() {
-		System.out.println("This method counts to twenty by twos using a for loop and a if statement!");
+		System.out.println("This method counts to twenty by twos using a for loop and a if statement:");
 		for (int i = 1; i < 21; i++) {
 			if (i % 2 == 0) {
-				System.out.println(i);
+				System.out.print(i + " ");
 			}
 		}
 	}
